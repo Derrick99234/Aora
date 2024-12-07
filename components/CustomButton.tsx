5a1ae5ab2c598bd12dc6da65de2edd521630fc0e
Derrick <pshubomi@gmail.com>
2024-12-07 18:54:21 +0100

    working on the welcome screen

---
+++ components/CustomButton.tsx
@@ -0,0 +1,25 @@
+import { Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+interface CustomButtonProps {
+    title: string
+    handlePress: () => void
+    containerStyles?: string
+    textStyles?: string
+    isLoading?: boolean
+  
+}
+const CustomButton = ({title, handlePress, containerStyles, textStyles, isLoading}: CustomButtonProps) => {
+  return (
+      <TouchableOpacity 
+        className={`bg-secondary-200 rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ""}`}
+        onPress={handlePress}
+        activeOpacity={0.7}
+        disabled={isLoading}
+      >
+        <Text className='text-primary font-psemibold text-lg'>{title}</Text>
+      </TouchableOpacity>
+  )
+}
+
+export default CustomButton
